e1d6f58c79036564bb4639e29b5fc49658f745e8
Gedearya <igd.ad.pratama@gmail.com>
2025-12-06 18:25:52 +0700

    Add additional console logs and conditional check for even/odd number

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ age ? console.log("You are an adult.") : console.log("You are a minor.");
 const name = () => {
   return "John Doe";
 };
+
+const keren = true;
+
+console.log(typeof greeting);
+console.log(typeof number);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof keren);
+
+let angka = 8;
+if (angka / 2) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+
+// comment this line
