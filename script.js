231b0b77ab52ea47e4621c6c92a42ffe0f889b9b
Gedearya <igd.ad.pratama@gmail.com>
2025-12-06 18:11:31 +0700

    Add initial JavaScript script with greeting and basic operations

---
+++ script.js
@@ -0,0 +1,13 @@
+var greeting = "Hello, World!";
+console.log(greeting);
+
+let number = 10;
+number += 5;
+console.log(number);
+
+const age = 25;
+age ? console.log("You are an adult.") : console.log("You are a minor.");
+
+const name = () => {
+  return "John Doe";
+};
